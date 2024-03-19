19c07b9eea08dd837556cad707ac13d935375fab
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2024-03-19 11:59:31 +0600

    first commit

---
+++ src/Components/Routes/Route.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../Shared/MainLayout";
+import Home from "../Home/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+const Route = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default Route;
